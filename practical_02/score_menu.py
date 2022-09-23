225b120841ca9b4e1b15197504c75170c93dded6
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-09-23 14:49:38 +1000

    Add menu functionality

--- practical_02/score_menu.py
+++ practical_02/score_menu.py
@@ -3,15 +3,21 @@ Determine score status and print as many stars as the score
 """
 
 
-from random import randint
+MENU = """S - Input score (0 - 100)
+Q - Quit"""
 
 
 def main():
-    score = float(input("Enter score: "))
-
-    result = return_score_result(score)
-    print(result)
-    print('*' * round(score))
+    print(MENU)
+    user_choice = input(">>> ")
+    while user_choice != "Q":
+        score = float(input("Enter score: "))
+        result = return_score_result(score)
+        print(result)
+        print('*' * round(score))
+        print(MENU)
+        user_choice = input(">>> ")
+    print("Thank you")
 
 
 def return_score_result(score):
