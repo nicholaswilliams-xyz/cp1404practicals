f7d480a2883b108bd266de6be7812bb12da10ce1
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-10-01 22:11:13 +1000

    Print a decimal number baesd on inputs Error check using try and except statements

---
+++ exceptions_demo.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+# Question 1:
+#   A "ValueError" will occur when a non-integer element is entered as an input
+# Question 2:
+#   A "ZeroDivisionError" will occur when the denominator is 0
+# Question 3:
+#   Please see above while loop
+
