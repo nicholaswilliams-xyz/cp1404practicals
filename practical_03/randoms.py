3b403a8032f6f18db770b850a66a199ee02d74a2
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-10-01 21:01:10 +1000

    Import random Print randomly generated number

---
+++ practical_03/randoms.py
@@ -0,0 +1,25 @@
+"""
+Answers to questions
+"""
+
+
+# Question 1:
+#   Smallest number possible: 5
+#   Largest number possible: 20
+
+# Question 2:
+#   Smallest number possible: 3
+#   Largest number possible: 9
+#   Line 2 cannot produce a 4, for
+#   the sum of an odd number (starting number of 3) and
+#   even number (incrementer of 2) always
+#   produces an odd number
+
+# Question 3:
+#   Smallest number possible: 2.5
+#   Largest number possible: 5.5 (depending on rounding)
+
+# Question 4:
+import random
+random_number = random.randint(0, 100)
+print(random_number)
