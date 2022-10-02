29af9fcaedde77049ba7a33f027d80c83561581b
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-10-02 21:19:30 +1000

    Add function docstrings

--- practical_03/files.py
+++ practical_03/files.py
@@ -4,6 +4,7 @@ Open file and read contents
 
 
 def main():
+    """Run all below functions on call"""
     create_name_file()
     read_name_file()
     read_and_add_two_numbers()
@@ -11,6 +12,7 @@ def main():
 
 
 def create_name_file():
+    """Create a file in which the contents of 'user_name' is writen"""
     user_name = input("Enter name: ")
     out_file = open("name.txt", 'w')
     print(f"{user_name}", file=out_file)
@@ -18,6 +20,7 @@ def create_name_file():
 
 
 def read_name_file():
+    """Open file created in 'create_name_file()' and print f-string"""
     in_file = open("name.txt", 'r')
     line = in_file.read()
     in_file.close()
@@ -25,6 +28,7 @@ def read_name_file():
 
 
 def read_and_add_two_numbers():
+    """Open 'numbers.txt' for reading and sum first two lines' numbers"""
     in_file = open("numbers.txt", 'r')
     sum_first_two_nums = 0
     for i in range(2):
@@ -35,6 +39,7 @@ def read_and_add_two_numbers():
 
 
 def read_and_add_all_numbers():
+    """Open 'numbers.txt' and sum all lines' numbers"""
     in_file = open("numbers.txt", 'r')
     sum_all_nums = 0
     for line in in_file:
