167d3fcef89c6ad5aa3d40e8902556735dcc52b9
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-10-31 18:50:44 +1000

    Import programming_language and add copy lines into program

---
+++ practical_06/languages.py
@@ -0,0 +1,14 @@
+"""
+
+Start date: 31.10.2022
+Start time: 18:45
+Estimated completion time: 30 minutes
+Actual completion time:
+"""
+
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
