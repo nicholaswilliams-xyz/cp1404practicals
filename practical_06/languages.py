82139fb80395ef4dbf4f98fc6e8be19fe551fad8
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-10-31 19:16:54 +1000

    Print languages that are only statically typed

--- practical_06/languages.py
+++ practical_06/languages.py
@@ -12,3 +12,10 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
+
+programming_languages = [python, ruby, visual_basic]
+
+print("The dynamically typed languages are:")
+for language in programming_languages:
+    if language.is_dynamic():
+        print(language.name)
