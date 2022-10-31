70ae52de9dc5ebf35549edf459cf145553bc8c5d
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-10-31 18:42:31 +1000

    Add __init__ to ProgrammingLanguages class

---
+++ practical_06/programming_language.py
@@ -0,0 +1,15 @@
+"""
+
+Start date: 31.10.2022
+Start time: 18:25
+Estimated completion time: 30 minutes
+"""
+
+
+class ProgrammingLanguage:
+    """Represent a programming language object"""
+
+    def __init__(self, typing="", reflection="", year=0):
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
