06934c11e05203bb876c0a1d502b375c80e626f9
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-11-07 17:36:19 +1000

    Get guitar from user, write guitars to csv file

--- practical_07/myguitars.py
+++ practical_07/myguitars.py
@@ -13,18 +13,31 @@ COST_INDEX = 2
 def main():
     """Read file of guitars, save as objects, display."""
     guitars = []
-
     in_file = open(GUITARS, 'r')
-
     for line in in_file:
         parts = line.strip().split(',')
         guitar = Guitar(parts[NAME_INDEX], int(parts[YEAR_INDEX]), float(parts[COST_INDEX]))
         guitars.append(guitar)
     in_file.close()
-    
+
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+
+    print("\nAdd a new guitar? (Enter to skip)")
+    name = input("Name: ")
+    while name != "":
+        age = input("Age: ")
+        cost = input("cost: ")
+        guitar = Guitar(name, int(age), float(cost))
+        guitars.append(guitar)
+        name = input("Name: ")
+
+    out_file = open(GUITARS, 'w')
+    for guitar in guitars:
+        print(guitar)
+        print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+    out_file.close()
 
 
 if __name__ == "__main__":
