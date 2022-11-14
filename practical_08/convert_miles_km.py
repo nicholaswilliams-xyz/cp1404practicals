51842205d1fced3ce6458716f1761b1bab91e78a
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-11-14 13:50:06 +1000

    Convert mi to km

---
+++ practical_08/convert_miles_km.py
@@ -0,0 +1,52 @@
+"""
+Convert miles to kilometres
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = 'Nicholas Williams'
+
+
+class MilesKilometreApp(App):
+    """MilesKilometreApp is a Kivy App for converting miles to kilometres"""
+
+    def build(self):
+        """Build the Kivy app from the kv file"""
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+    def up(self):
+        """Handle up"""
+        miles = self.root.ids.input_number.text
+        try:
+            miles = float(miles) + 1
+            self.root.ids.input_number.text = str(miles)
+        except ValueError:
+            pass
+
+    def down(self):
+        """Handle up"""
+        miles = self.root.ids.input_number.text
+        try:
+            miles = float(miles) - 1
+            if miles < 0:
+                # Disable negative distances
+                miles = 0.0
+            self.root.ids.input_number.text = str(miles)
+        except ValueError:
+            pass
+
+    def convert(self):
+        """Handle the conversion of miles to kilometres"""
+        miles = self.root.ids.input_number.text
+        try:
+            kilometres = float(miles) * 1.609
+            self.root.ids.kilometres_value.text = str(round(kilometres, 3))
+        except ValueError:
+            pass
+
+
+MilesKilometreApp().run()
