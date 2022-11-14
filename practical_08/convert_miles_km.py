a1a2daadee9870a7cbed8d50266c7dc206baed4e
NonNPCNick <109601511+NonNPCNick@users.noreply.github.com>
2022-11-14 14:21:40 +1000

    Convert up() and down() functions into one increment() function. Make miles to km scale a constant.

--- practical_08/convert_miles_km.py
+++ practical_08/convert_miles_km.py
@@ -8,6 +8,8 @@ from kivy.core.window import Window
 
 __author__ = 'Nicholas Williams'
 
+MILES_TO_KM_SCALE = 1.609
+
 
 class MilesKilometreApp(App):
     """MilesKilometreApp is a Kivy App for converting miles to kilometres"""
@@ -18,23 +20,14 @@ class MilesKilometreApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def up(self):
-        """Handle up"""
+    def increment(self, value):
+        """ Increment by "value" """
         miles = self.root.ids.input_number.text
         try:
-            miles = float(miles) + 1
-            self.root.ids.input_number.text = str(miles)
-        except ValueError:
-            pass
-
-    def down(self):
-        """Handle up"""
-        miles = self.root.ids.input_number.text
-        try:
-            miles = float(miles) - 1
+            miles = float(miles) + value
             if miles < 0:
                 # Disable negative distances
-                miles = 0.0
+                miles = 0
             self.root.ids.input_number.text = str(miles)
         except ValueError:
             pass
@@ -43,10 +36,10 @@ class MilesKilometreApp(App):
         """Handle the conversion of miles to kilometres"""
         miles = self.root.ids.input_number.text
         try:
-            kilometres = float(miles) * 1.609
+            kilometres = float(miles) * MILES_TO_KM_SCALE
             self.root.ids.kilometres_value.text = str(round(kilometres, 3))
         except ValueError:
-            self.root.ids.input_number.text = str(0.0)
+            self.root.ids.input_number.text = str('')
             self.root.ids.kilometres_value.text = str(0.0)
             pass
 
