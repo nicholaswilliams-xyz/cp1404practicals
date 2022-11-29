9a24da7f538e06212feba1ed7da77a5aabe845a4
nicholaswilliams-xyz <109601511+nicholaswilliams-xyz@users.noreply.github.com>
2022-11-29 23:03:47 +1000

    Add DisambiguationError handling

--- practical_10/wiki.py
+++ practical_10/wiki.py
@@ -8,12 +8,15 @@ def main():
     search_phrase = get_search_phrase()
     while search_phrase != "":
         try:
-            search_phrase_page = wikipedia.page(search_phrase)
+            search_phrase_page = wikipedia.page(search_phrase, auto_suggest=False)
             print(f"  Title: {search_phrase_page.title}")
             print(f"    URL: {search_phrase_page.url}")
             print(f"Summary: {wikipedia.summary(search_phrase)}")
         except wikipedia.exceptions.PageError:
             print(f"Page id {search_phrase} does not match any pages. Try another id!")
+        except wikipedia.exceptions.DisambiguationError as e:
+            print("Disambiguation error: please enter a more specific search phrase (see suggestions below):")
+            print(e.options)
         print('')
         search_phrase = get_search_phrase()
 
