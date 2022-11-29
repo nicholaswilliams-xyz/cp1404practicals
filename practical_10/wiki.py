7459f16b3f60e8be30984cab5f2e365e4086cdb5
nicholaswilliams-xyz <109601511+nicholaswilliams-xyz@users.noreply.github.com>
2022-11-29 22:56:36 +1000

    Print page title and URL

--- practical_10/wiki.py
+++ practical_10/wiki.py
@@ -8,9 +8,13 @@ def main():
     search_phrase = get_search_phrase()
     while search_phrase != "":
         try:
-            print(wikipedia.summary(search_phrase))
+            search_phrase_page = wikipedia.page(search_phrase)
+            print(f"  Title: {search_phrase_page.title}")
+            print(f"    URL: {search_phrase_page.url}")
+            print(f"Summary: {wikipedia.summary(search_phrase)}")
         except wikipedia.exceptions.PageError:
             print(f"Page id {search_phrase} does not match any pages. Try another id!")
+        print('')
         search_phrase = get_search_phrase()
 
 
